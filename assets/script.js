48af0bd8e3e7d784dc193ca5b78f70c0ca4fab90
Omar-Arja <omaralarja17@gmail.com>
2023-07-13 13:33:50 +0300

    reformatted and removed unnecessary lines

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
 const toggle = document.getElementById('toggle');
 let dropdown = document.getElementById('dropdown-Menu');
-let container = document.getElementById('cont');
-let header = document.getElementById('header');
 
 toggle.addEventListener('click', function () {
     dropdown.classList.toggle('dropdown-Menu-show');
